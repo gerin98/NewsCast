a40bc5a37d7d49558e14cb3e469e00b341f99c46
Gerin Amalaraj <gerin.amalaraj@wishabi.com>
2020-06-08 10:36:58 -0400

    finalized navigation view design

--- app/src/main/java/com/example/newscast/ui/browse/MainActivity.kt
+++ app/src/main/java/com/example/newscast/ui/browse/MainActivity.kt
@@ -2,13 +2,14 @@ package com.example.newscast.ui.browse
 
 import android.os.Bundle
 import android.util.Log
-import android.view.Gravity
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -21,8 +22,10 @@ import com.example.newscast.ui.adapter.NewsAdapter
 import com.google.android.material.navigation.NavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
-
-class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+class MainActivity : AppCompatActivity(),
+    NavigationView.OnNavigationItemSelectedListener,
+    Toolbar.OnMenuItemClickListener,
+    View.OnClickListener {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
@@ -43,43 +46,25 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         viewManager = LinearLayoutManager(this)
         viewAdapter = NewsAdapter(myDataset)
 
-        recyclerView = findViewById<RecyclerView>(R.id.newsRecyclerView).apply {
-            setHasFixedSize(true)
-            layoutManager = viewManager
-            adapter = viewAdapter
-        }
-
-        val dividerItemDecoration = DividerItemDecoration(recyclerView.context, LinearLayout.VERTICAL).apply {
+        val dividerItemDecoration = DividerItemDecoration(this, LinearLayout.VERTICAL).apply {
             this@MainActivity.getDrawable(R.drawable.divider)?.let {
                 setDrawable(it)
             }
         }
-        recyclerView.addItemDecoration(dividerItemDecoration)
 
-        news_button.setOnClickListener{
-            viewModel.getLatestNews()
-        }
-
-        newsBottomAppBar.setOnMenuItemClickListener {item ->
-            when(item.itemId) {
-                R.id.menu_favourites -> {
-                    // do something
-                    true
-                }
-                R.id.menu_search -> {
-                    // do something
-                    true
-                }
-                else -> false
-            }
-
-        }
-
-        newsBottomAppBar.setNavigationOnClickListener {item ->
-            drawerLayout.openDrawer(GravityCompat.START)
+        recyclerView = findViewById<RecyclerView>(R.id.newsRecyclerView).apply {
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+            addItemDecoration(dividerItemDecoration)
         }
 
         navigationView.setNavigationItemSelectedListener(this)
+        news_button.setOnClickListener(this)
+        newsBottomAppBar.setOnMenuItemClickListener(this)
+        newsBottomAppBar.setNavigationOnClickListener {
+            drawerLayout.openDrawer(GravityCompat.START)
+        }
 
         initLiveData()
     }
@@ -91,14 +76,77 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-//         Handle item selection
+        // Handle item selection
         return when (item.itemId) {
-            R.id.menu_settings -> {
-                Toast.makeText(this, "Settings clicked", Toast.LENGTH_SHORT).show()
+            R.id.menu_world -> {
+                Toast.makeText(this, "World clicked", Toast.LENGTH_SHORT).show()
+                drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
+            R.id.menu_us -> {
+                Toast.makeText(this, "US clicked", Toast.LENGTH_SHORT).show()
+                drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
+            R.id.menu_politics -> {
+                Toast.makeText(this, "Politics clicked", Toast.LENGTH_SHORT).show()
+                drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
+            R.id.menu_business -> {
+                Toast.makeText(this, "Business clicked", Toast.LENGTH_SHORT).show()
+                drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
+            R.id.menu_tech -> {
+                Toast.makeText(this, "Tech clicked", Toast.LENGTH_SHORT).show()
+                drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
+            R.id.menu_science -> {
+                Toast.makeText(this, "Science clicked", Toast.LENGTH_SHORT).show()
+                drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
+            R.id.menu_sports -> {
+                Toast.makeText(this, "Sports clicked", Toast.LENGTH_SHORT).show()
+                drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
+            R.id.menu_travel -> {
+                Toast.makeText(this, "Travel clicked", Toast.LENGTH_SHORT).show()
+                drawerLayout.closeDrawer(GravityCompat.START)
+                true
+            }
+            R.id.menu_culture -> {
+                Toast.makeText(this, "Culture clicked", Toast.LENGTH_SHORT).show()
                 drawerLayout.closeDrawer(GravityCompat.START)
                 true
             }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onMenuItemClick(item: MenuItem?): Boolean {
+        return when(item?.itemId) {
+            R.id.menu_favourites -> {
+                // do something
+                true
+            }
+            R.id.menu_search -> {
+                // do something
+                true
+            }
+            else -> false
+        }
+    }
+
+    override fun onClick(view: View?) {
+        when(view?.id) {
+            R.id.news_button -> {
+                viewModel.getLatestNews()
+            }
+            else -> {}
         }
     }
 
